da8c52cdb1e1de36cf8d110e34466f81e2c3bf7f
Raúl Adamuz <radamuz16@gmail.com>
2021-01-20 23:34:41 +0100

    bug index out of range solved and yRL100 added

--- ytRandomLinks100.py
+++ ytRandomLinks100.py
@@ -5,18 +5,18 @@ import subprocess
 ascii_char = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z', '_','-']
 
 # Making a variable that we can use to enter in while loop
-x = ''
+x = 0
 
-while x == '':
-
+while x < 100:
+    x += 1
     # Array of created links
     created_links = []
     
     # User input
-    x = input('Hit enter to generate a random Youtube link: ')
+    #x = input('Hit enter to generate a random Youtube link: ')
 
-    if x != '':
-        break
+    # if x != '':
+    #     break
 
     str = ''
     for i in range(11):
